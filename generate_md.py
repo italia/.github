261df0cfb3c1d40a37ac7659a488cc9e9656a4a2
Andrea Stagi <stagi.andrea@gmail.com>
2024-12-16 12:30:12 +0100

    chore: linting

--- generate_md.py
+++ generate_md.py
@@ -3,37 +3,42 @@ from jinja2 import Environment, FileSystemLoader, select_autoescape
 from slugify import slugify
 from utils import auth_to_github, get_inactive_repos, write_inactive_repos_to_md
 
-ORGANIZATIONS = ['italia', 'teamdigitale']
+ORGANIZATIONS = ["italia", "teamdigitale"]
 INACTIVE_DAYS_TRESHOLD = 365
+
 
 def load_groups_and_repos():
     groups = []
     repositories = {}
-    with open(os.path.join('data', 'groups.json')) as f:
+    with open(os.path.join("data", "groups.json")) as f:
         groups = json.load(f)
-    with open(os.path.join('data', 'repositories.json')) as f:
+    with open(os.path.join("data", "repositories.json")) as f:
         repositories = json.load(f)
     inactive_repos = []
     all_repos = []
 
     for organization in ORGANIZATIONS:
-        inactive_repos.extend(get_inactive_repos(gh_connection, INACTIVE_DAYS_TRESHOLD, organization))
+        inactive_repos.extend(
+            get_inactive_repos(gh_connection, INACTIVE_DAYS_TRESHOLD, organization)
+        )
         all_repos.extend(gh_connection.organization(organization).repositories())
 
     write_inactive_repos_to_md(inactive_repos, INACTIVE_DAYS_TRESHOLD)
-    inactive_repos_urls = [repo['url'] for repo in inactive_repos]
+    inactive_repos_urls = [repo["url"] for repo in inactive_repos]
 
     for repo in all_repos:
         if repo.html_url not in inactive_repos_urls:
             if repo.archived or repo.private:
                 if repo.html_url in repositories:
-                    del repositories[repo.html_url]  
-                continue              
+                    del repositories[repo.html_url]
+                continue
             if not repo.html_url in repositories:
-                repositories[repo.html_url] = ''
+                repositories[repo.html_url] = ""
             else:
                 if repositories[repo.html_url]:
-                    group = [gr for gr in groups if gr['id'] == repositories[repo.html_url]][0]
+                    group = [
+                        gr for gr in groups if gr["id"] == repositories[repo.html_url]
+                    ][0]
                     ## Init group
                     if not "repos" in group:
                         group["repos"] = []
@@ -41,29 +46,31 @@ def load_groups_and_repos():
                         group["slug"] = slugify(group["name"])
                     group["repos"].append(
                         {
-                            'url' : repo.html_url,
-                            'slug' : repo.name,
-                            'description': repo.description or '',
-                            'stars' : repo.stargazers_count,
-                            'organization': repo.html_url.replace('https://github.com/', '').split('/')[0]
+                            "url": repo.html_url,
+                            "slug": repo.name,
+                            "description": repo.description or "",
+                            "stars": repo.stargazers_count,
+                            "organization": repo.html_url.replace(
+                                "https://github.com/", ""
+                            ).split("/")[0],
                         }
                     )
-    with open(os.path.join('data', 'repositories.json'), 'w', encoding='utf-8') as f:
+    with open(os.path.join("data", "repositories.json"), "w", encoding="utf-8") as f:
         json.dump(repositories, f, ensure_ascii=False, indent=4)
     return groups
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     gh_connection = auth_to_github()
 
     groups = load_groups_and_repos()
 
     env = Environment(
-        loader=FileSystemLoader(searchpath=os.path.join('templates')),
-        autoescape=select_autoescape()
+        loader=FileSystemLoader(searchpath=os.path.join("templates")),
+        autoescape=select_autoescape(),
     )
 
-    template = env.get_template(os.path.join('main.md'))
+    template = env.get_template(os.path.join("main.md"))
 
-    with open(os.path.join('README.md'), "w") as f:
+    with open(os.path.join("README.md"), "w") as f:
         f.write(template.render(groups=groups))
